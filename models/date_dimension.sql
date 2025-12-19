02ab8a4d517c1a08ad3e4439e2e7648101b31fc3
Howard S <iluvsushi9999@hotmail.com>
2025-12-19 19:58:54 +0000

    daily_weather model created

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -4,21 +4,10 @@ select
 to_timestamp(started_at) as started_at,
 DATE(to_timestamp(started_at)) as DATE_STARTED_AT,
 hour(to_timestamp(started_at)) as hour_started_at,
-CASE
-when dayname(to_timestamp(started_at)) in ('Sat', 'Sun')
-then 'WEEKEND'
-else 'BusinessDay'
-end as DayType,
-case
-    when month(to_timestamp(started_at)) in (12, 1, 2,3)
-    then 'WINTER'
-    when month(to_timestamp(started_at)) in (4, 5)
-    then 'SPING'
-    when month(to_timestamp(started_at)) in (6, 7, 8)
-    then 'SUMMER'
-else 'FALL'
-end as Season
 
+{{day_type('started_at')}} as day_type,
+
+{{ get_season('started_at') }} AS season
 
 from 
 {{ source('demo', 'bike') }}
