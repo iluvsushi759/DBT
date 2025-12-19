0f99656a09bff655324ede47eb2b89eb51fa966d
Howard S <iluvsushi9999@hotmail.com>
2025-12-19 22:35:34 +0000

    station_dimension model created

---
+++ models/station_dimension.sql
@@ -0,0 +1,14 @@
+with bike as (
+
+    select distinct start_statio_id as start_station_id,
+    start_station_name as station_name,
+    start_lat as stationi_lat,
+    start_lng as start_station_lng
+
+    from {{source('demo', 'bike')}}
+    where ride_ID != 'ride_id'
+   
+
+)
+
+select * from bike
